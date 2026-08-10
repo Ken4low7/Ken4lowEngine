f22dde480a8cc6fc24b459734c02ab4649fb78bb
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-10 17:56:26 +0900

    Level Version Migrationの入口を追加

---
+++ Project/Engine/Scene/Level/LevelVersionMigration.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <json.hpp>
+
+#include <cstdint>
+#include <string>
+
+namespace Ken4lowEngine
+{
+	/// <summary>Ken4lowLevelの旧Versionを現在Versionへ順番に変換する。</summary>
+	class LevelVersionMigration
+	{
+	public:
+		static bool MigrateToCurrent(nlohmann::json& levelJson, uint32_t& outSourceVersion, std::string& outError);
+
+	private:
+		static bool MigrateVersion1To2(nlohmann::json& levelJson, std::string& outError);
+	};
+} // namespace Ken4lowEngine
