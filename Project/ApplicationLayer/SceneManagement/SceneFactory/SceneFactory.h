1248bbd5d415c8b40c6eaca28f10c50d1bc71782
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-13 20:46:53 +0900

    feat: add scene self registration

--- Project/ApplicationLayer/SceneManagement/SceneFactory/SceneFactory.h
+++ Project/ApplicationLayer/SceneManagement/SceneFactory/SceneFactory.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "AbstractSceneFactory.h"
 
+#include <functional>
+#include <string>
+#include <vector>
+
 namespace Ken4lowEngine
 {
 	/// -------------------------------------------------------------
@@ -8,14 +12,24 @@ namespace Ken4lowEngine
 	/// -------------------------------------------------------------
 	class SceneFactory : public AbstractSceneFactory
 	{
-	public: /// ---------- メンバ関数 ---------- ///
+	public:
+		using SceneCreator = std::function<std::unique_ptr<BaseScene>()>;
 
-		/// <summary>
-		/// シーン生成
-		/// </summary>
-		/// <param name="sceneName">シーン名</param>
-		/// <returns>生成したシーン</returns>
+		/// <summary>Scene ClassをFactoryへ登録します。</summary>
+		static bool RegisterSceneClass(std::string sceneName, SceneCreator creator);
+
+		/// <summary>登録済みScene ClassからSceneを生成します。</summary>
 		std::unique_ptr<BaseScene> CreateScene(const std::string& sceneName) override;
+
+		bool CanCreateScene(const std::string& sceneName) const override;
+		std::vector<std::string> GetRegisteredSceneNames() const override;
 	};
 
-}
+} // namespace Ken4lowEngine
+
+#define K4E_SCENE_REGISTRY_CONCAT_IMPL(lhs, rhs) lhs##rhs
+#define K4E_SCENE_REGISTRY_CONCAT(lhs, rhs) K4E_SCENE_REGISTRY_CONCAT_IMPL(lhs, rhs)
+#define K4E_REGISTER_SCENE_NAMED(SceneName, SceneType) \
+	namespace { [[maybe_unused]] const bool K4E_SCENE_REGISTRY_CONCAT(g_k4eSceneRegistered_, __COUNTER__) = \
+		::Ken4lowEngine::SceneFactory::RegisterSceneClass(SceneName, [] { return std::make_unique<SceneType>(); }); }
+#define K4E_REGISTER_SCENE(SceneType) K4E_REGISTER_SCENE_NAMED(#SceneType, SceneType) // Scene側に1行書くだけでFactoryとEditor一覧へ自動登録する。
