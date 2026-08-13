593d1035db9c7ed125efc5ad6f5b923196a77613
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-13 20:46:33 +0900

    feat: expose registered scene classes

--- Project/Engine/Scene/Management/AbstractSceneFactory.h
+++ Project/Engine/Scene/Management/AbstractSceneFactory.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace Ken4lowEngine
 {
@@ -25,6 +26,11 @@ namespace Ken4lowEngine
 		/// <returns>シーンを返す</returns>
 		virtual std::unique_ptr<BaseScene> CreateScene(const std::string& sceneName) = 0;
 
+		/// <summary>指定Class名をこのFactoryが生成できるか返します。</summary>
+		virtual bool CanCreateScene(const std::string& sceneName) const = 0;
+
+		/// <summary>EditorのScene一覧へ公開する登録済みClass名を返します。</summary>
+		virtual std::vector<std::string> GetRegisteredSceneNames() const = 0; // Scene一覧のハードコードをなくしFactory登録を唯一の情報源にする。
 	};
 
 }
