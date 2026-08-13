91e9c1c8d7fad14eba091a1cb465f84df1c9291d
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-13 13:20:29 +0900

    perf: expose mesh particle material to vertex shader

--- Project/Engine/Graphics/Renderer/GpuParticle/Pipeline/GpuParticleMeshPipeline.cpp
+++ Project/Engine/Graphics/Renderer/GpuParticle/Pipeline/GpuParticleMeshPipeline.cpp
@@ -42,33 +42,27 @@ void GpuParticleMeshPipeline::CreateRootSignature()
 	D3D12_ROOT_SIGNATURE_DESC rsDesc{};
 	rsDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 
-	// SRV Table（t0 を 1つ）
 	D3D12_DESCRIPTOR_RANGE srvRange[1]{};
 	srvRange[0].BaseShaderRegister = 0;
 	srvRange[0].NumDescriptors = 1;
 	srvRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
 	srvRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
 
-	// RootParameters（スプライトと同じ構成で開始）
 	D3D12_ROOT_PARAMETER params[4]{};
 
-	// b0 : PerView / 定数（全部のシェーダ）
 	params[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
 	params[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 	params[0].Descriptor.ShaderRegister = 0;
 
-	// t0 : Particle SRV（VS）
 	params[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
 	params[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
 	params[1].DescriptorTable.NumDescriptorRanges = _countof(srvRange);
 	params[1].DescriptorTable.pDescriptorRanges = srvRange;
 
-	// b1 : Material（PS）
 	params[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
-	params[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	params[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL; // Mesh VSでもMaterial groupを見て不要Instanceを早期除外する。
 	params[2].Descriptor.ShaderRegister = 1;
 
-	// t0 : Texture SRV（PS）
 	params[3].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
 	params[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
 	params[3].DescriptorTable.NumDescriptorRanges = _countof(srvRange);
@@ -77,7 +71,6 @@ void GpuParticleMeshPipeline::CreateRootSignature()
 	rsDesc.pParameters = params;
 	rsDesc.NumParameters = _countof(params);
 
-	// Static Sampler（スプライトと同じで開始）
 	D3D12_STATIC_SAMPLER_DESC samplers[1]{};
 	samplers[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
 	samplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
@@ -108,7 +101,6 @@ void GpuParticleMeshPipeline::CreatePSO(BlendMode blendMode)
 {
 	HRESULT hr{};
 
-	// ---- 入力レイアウト（まずはスプライトと同じにしておく）----
 	D3D12_INPUT_ELEMENT_DESC input[3]{};
 	input[0] = { "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
 	input[1] = { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0,	   D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
@@ -121,12 +113,10 @@ void GpuParticleMeshPipeline::CreatePSO(BlendMode blendMode)
 	// Mesh ParticleもSpriteと同じAuthoring BlendMode契約を使う。
 	const D3D12_RENDER_TARGET_BLEND_DESC blendDesc = BlendStateFactory::GetInstance()->GetBlendDesc(blendMode);
 
-	// ---- Rasterizer（メッシュなので基本BACK。粒子用途で両面欲しければNONE）----
 	D3D12_RASTERIZER_DESC rast{};
 	rast.CullMode = D3D12_CULL_MODE_BACK;
 	rast.FillMode = D3D12_FILL_MODE_SOLID;
 
-	// ---- Depth（推奨：テストON・書き込みOFF）----
 	D3D12_DEPTH_STENCIL_DESC ds{};
 	ds.DepthEnable = TRUE;
 	ds.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
@@ -152,7 +142,6 @@ void GpuParticleMeshPipeline::CreatePSO(BlendMode blendMode)
 		dxCommon_->GetDXCCompilerManager());
 	assert(ps != nullptr);
 
-	// ---- PSO ----
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC desc{};
 	desc.pRootSignature = rootSignature_.Get();
 	desc.InputLayout = layout;
@@ -160,13 +149,10 @@ void GpuParticleMeshPipeline::CreatePSO(BlendMode blendMode)
 	desc.PS = { ps->GetBufferPointer(), ps->GetBufferSize() };
 	desc.BlendState.RenderTarget[0] = blendDesc;
 	desc.RasterizerState = rast;
-
 	desc.DepthStencilState = ds;
 	desc.DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
-
 	desc.NumRenderTargets = 1;
 	desc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
-
 	desc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 	desc.SampleDesc.Count = 1;
 	desc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
