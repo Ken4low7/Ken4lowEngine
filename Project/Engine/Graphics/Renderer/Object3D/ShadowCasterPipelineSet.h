a71b9132b455afeeb6b9b100e71b76e647fd5e54
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-14 20:41:34 +0900

    feat: add material cull modes to shadow casters

--- Project/Engine/Graphics/Renderer/Object3D/ShadowCasterPipelineSet.h
+++ Project/Engine/Graphics/Renderer/Object3D/ShadowCasterPipelineSet.h
@@ -7,6 +7,7 @@
 #include "PipelineFactory.h"
 #include "PipelineStatePresets.h"
 #include "ShaderCompiler.h"
+#include "Material.h"
 
 #include <array>
 #include <cassert>
@@ -41,26 +42,72 @@ namespace Ken4lowEngine
 			const ComPtr<IDxcBlob> instancedPointVsBlob = ShaderCompiler::CompileShader(instancedPointVs, dxcManager);
 			const ComPtr<IDxcBlob> pointPsBlob = ShaderCompiler::CompileShader(pointPs, dxcManager);
 
-			CreateObjectDepthPipeline(factory, inputLayout, objectDepthVsBlob);
-			CreateInstancedDepthPipeline(factory, inputLayout, instancedDepthVsBlob);
-			CreateObjectPointPipeline(factory, inputLayout, objectPointVsBlob, pointPsBlob);
-			CreateInstancedPointPipeline(factory, inputLayout, instancedPointVsBlob, pointPsBlob);
+			constexpr std::array<MaterialCullMode, 3> cullModes{
+				MaterialCullMode::Back,
+				MaterialCullMode::Front,
+				MaterialCullMode::None,
+			};
+			for (size_t i = 0; i < cullModes.size(); ++i)
+			{
+				CreateObjectDepthPipeline(factory, inputLayout, objectDepthVsBlob, cullModes[i], objectDepthPipelines_[i]);
+				CreateInstancedDepthPipeline(factory, inputLayout, instancedDepthVsBlob, cullModes[i], instancedDepthPipelines_[i]);
+				CreateObjectPointPipeline(factory, inputLayout, objectPointVsBlob, pointPsBlob, cullModes[i], objectPointPipelines_[i]);
+				CreateInstancedPointPipeline(factory, inputLayout, instancedPointVsBlob, pointPsBlob, cullModes[i], instancedPointPipelines_[i]);
+			}
 		}
 
 		void Finalize()
 		{
-			instancedPointPipeline_.Reset();
-			objectPointPipeline_.Reset();
-			instancedDepthPipeline_.Reset();
-			objectDepthPipeline_.Reset();
+			for (PipelineBundle& pipeline : instancedPointPipelines_) pipeline.Reset();
+			for (PipelineBundle& pipeline : objectPointPipelines_) pipeline.Reset();
+			for (PipelineBundle& pipeline : instancedDepthPipelines_) pipeline.Reset();
+			for (PipelineBundle& pipeline : objectDepthPipelines_) pipeline.Reset();
 		}
 
-		const PipelineBundle& GetObjectDepth() const { return objectDepthPipeline_; }
-		const PipelineBundle& GetInstancedDepth() const { return instancedDepthPipeline_; }
-		const PipelineBundle& GetObjectPoint() const { return objectPointPipeline_; }
-		const PipelineBundle& GetInstancedPoint() const { return instancedPointPipeline_; }
+		const PipelineBundle& GetObjectDepth(MaterialCullMode cullMode = MaterialCullMode::Back) const { return Select(objectDepthPipelines_, cullMode); }
+		const PipelineBundle& GetInstancedDepth(MaterialCullMode cullMode = MaterialCullMode::Back) const { return Select(instancedDepthPipelines_, cullMode); }
+		const PipelineBundle& GetObjectPoint(MaterialCullMode cullMode = MaterialCullMode::Back) const { return Select(objectPointPipelines_, cullMode); }
+		const PipelineBundle& GetInstancedPoint(MaterialCullMode cullMode = MaterialCullMode::Back) const { return Select(instancedPointPipelines_, cullMode); }
 
 	private:
+		static size_t ToIndex(MaterialCullMode cullMode)
+		{
+			switch (cullMode)
+			{
+			case MaterialCullMode::Front: return 1;
+			case MaterialCullMode::None: return 2;
+			case MaterialCullMode::Back:
+			default: return 0;
+			}
+		}
+
+		static const PipelineBundle& Select(const std::array<PipelineBundle, 3>& pipelines, MaterialCullMode cullMode)
+		{
+			return pipelines[ToIndex(cullMode)];
+		}
+
+		static D3D12_RASTERIZER_DESC MakeShadowRasterizer(MaterialCullMode cullMode)
+		{
+			D3D12_RASTERIZER_DESC rasterizer{};
+			switch (cullMode)
+			{
+			case MaterialCullMode::Front:
+				rasterizer = PipelineStatePresets::MakeRasterizerCullFront();
+				break;
+			case MaterialCullMode::None:
+				rasterizer = PipelineStatePresets::MakeRasterizerCullNone();
+				break;
+			case MaterialCullMode::Back:
+			default:
+				rasterizer = PipelineStatePresets::MakeRasterizerCullBack();
+				break;
+			}
+			rasterizer.DepthBias = 600;
+			rasterizer.SlopeScaledDepthBias = 1.25f;
+			rasterizer.DepthBiasClamp = 0.0025f;
+			return rasterizer;
+		}
+
 		static std::array<D3D12_INPUT_ELEMENT_DESC, 3> MakeInputLayout()
 		{
 			return {
@@ -70,14 +117,11 @@ namespace Ken4lowEngine
 			};
 		}
 
-		static GraphicsPipelineDesc MakeDepthDesc(const D3D12_INPUT_LAYOUT_DESC& inputLayout)
+		static GraphicsPipelineDesc MakeDepthDesc(const D3D12_INPUT_LAYOUT_DESC& inputLayout, MaterialCullMode cullMode)
 		{
 			GraphicsPipelineDesc desc{};
 			desc.blendState = PipelineStatePresets::MakeBlendOpaque();
-			desc.rasterizerState = PipelineStatePresets::MakeRasterizerCullBack();
-			desc.rasterizerState.DepthBias = 600;
-			desc.rasterizerState.SlopeScaledDepthBias = 1.25f;
-			desc.rasterizerState.DepthBiasClamp = 0.0025f; // 表裏両面の競合を避けつつ、傾斜面だけ適度に深度を押し出す。
+			desc.rasterizerState = MakeShadowRasterizer(cullMode); // ShadowもMain Passと同じMaterial Surface契約を使う。
 			desc.depthStencilState = PipelineStatePresets::MakeDepthReadWrite();
 			desc.numRenderTargets = 0;
 			desc.dsvFormat = DXGI_FORMAT_D32_FLOAT;
@@ -88,13 +132,11 @@ namespace Ken4lowEngine
 			return desc;
 		}
 
-		static D3D12_ROOT_SIGNATURE_DESC MakeObjectDepthRoot(
-			std::array<D3D12_ROOT_PARAMETER, 1>& parameters)
+		static D3D12_ROOT_SIGNATURE_DESC MakeObjectDepthRoot(std::array<D3D12_ROOT_PARAMETER, 1>& parameters)
 		{
 			parameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
 			parameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
 			parameters[0].Descriptor.ShaderRegister = 0;
-
 			D3D12_ROOT_SIGNATURE_DESC desc{};
 			desc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 			desc.NumParameters = static_cast<UINT>(parameters.size());
@@ -102,24 +144,19 @@ namespace Ken4lowEngine
 			return desc;
 		}
 
-		static D3D12_ROOT_SIGNATURE_DESC MakeInstancedDepthRoot(
-			D3D12_DESCRIPTOR_RANGE& instanceRange,
-			std::array<D3D12_ROOT_PARAMETER, 2>& parameters)
+		static D3D12_ROOT_SIGNATURE_DESC MakeInstancedDepthRoot(D3D12_DESCRIPTOR_RANGE& instanceRange, std::array<D3D12_ROOT_PARAMETER, 2>& parameters)
 		{
 			instanceRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
 			instanceRange.NumDescriptors = 1;
 			instanceRange.BaseShaderRegister = 0;
 			instanceRange.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
-
 			parameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
 			parameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
 			parameters[0].Descriptor.ShaderRegister = 0;
-
 			parameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
 			parameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
 			parameters[1].DescriptorTable.NumDescriptorRanges = 1;
 			parameters[1].DescriptorTable.pDescriptorRanges = &instanceRange;
-
 			D3D12_ROOT_SIGNATURE_DESC desc{};
 			desc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 			desc.NumParameters = static_cast<UINT>(parameters.size());
@@ -127,17 +164,14 @@ namespace Ken4lowEngine
 			return desc;
 		}
 
-		static D3D12_ROOT_SIGNATURE_DESC MakeObjectPointRoot(
-			std::array<D3D12_ROOT_PARAMETER, 2>& parameters)
+		static D3D12_ROOT_SIGNATURE_DESC MakeObjectPointRoot(std::array<D3D12_ROOT_PARAMETER, 2>& parameters)
 		{
 			parameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
 			parameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
 			parameters[0].Descriptor.ShaderRegister = 0;
-
 			parameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
 			parameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 			parameters[1].Descriptor.ShaderRegister = 1;
-
 			D3D12_ROOT_SIGNATURE_DESC desc{};
 			desc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 			desc.NumParameters = static_cast<UINT>(parameters.size());
@@ -145,28 +179,22 @@ namespace Ken4lowEngine
 			return desc;
 		}
 
-		static D3D12_ROOT_SIGNATURE_DESC MakeInstancedPointRoot(
-			D3D12_DESCRIPTOR_RANGE& instanceRange,
-			std::array<D3D12_ROOT_PARAMETER, 3>& parameters)
+		static D3D12_ROOT_SIGNATURE_DESC MakeInstancedPointRoot(D3D12_DESCRIPTOR_RANGE& instanceRange, std::array<D3D12_ROOT_PARAMETER, 3>& parameters)
 		{
 			instanceRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
 			instanceRange.NumDescriptors = 1;
 			instanceRange.BaseShaderRegister = 0;
 			instanceRange.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
-
 			parameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
 			parameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
 			parameters[0].Descriptor.ShaderRegister = 0;
-
 			parameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
 			parameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
 			parameters[1].DescriptorTable.NumDescriptorRanges = 1;
 			parameters[1].DescriptorTable.pDescriptorRanges = &instanceRange;
-
 			parameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
 			parameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 			parameters[2].Descriptor.ShaderRegister = 1;
-
 			D3D12_ROOT_SIGNATURE_DESC desc{};
 			desc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 			desc.NumParameters = static_cast<UINT>(parameters.size());
@@ -174,54 +202,54 @@ namespace Ken4lowEngine
 			return desc;
 		}
 
-		void CreateObjectDepthPipeline(PipelineFactory& factory, const D3D12_INPUT_LAYOUT_DESC& inputLayout, const ComPtr<IDxcBlob>& vs)
+		void CreateObjectDepthPipeline(PipelineFactory& factory, const D3D12_INPUT_LAYOUT_DESC& inputLayout, const ComPtr<IDxcBlob>& vs, MaterialCullMode cullMode, PipelineBundle& destination)
 		{
 			std::array<D3D12_ROOT_PARAMETER, 1> parameters{};
 			const D3D12_ROOT_SIGNATURE_DESC root = MakeObjectDepthRoot(parameters);
-			GraphicsPipelineDesc desc = MakeDepthDesc(inputLayout);
+			GraphicsPipelineDesc desc = MakeDepthDesc(inputLayout, cullMode);
 			desc.debugName = L"ShadowCaster.Object.Depth";
 			desc.shaders.vertexShader.blob = vs;
-			objectDepthPipeline_ = factory.CreateGraphicsPipeline(desc, root);
+			destination = factory.CreateGraphicsPipeline(desc, root);
 		}
 
-		void CreateInstancedDepthPipeline(PipelineFactory& factory, const D3D12_INPUT_LAYOUT_DESC& inputLayout, const ComPtr<IDxcBlob>& vs)
+		void CreateInstancedDepthPipeline(PipelineFactory& factory, const D3D12_INPUT_LAYOUT_DESC& inputLayout, const ComPtr<IDxcBlob>& vs, MaterialCullMode cullMode, PipelineBundle& destination)
 		{
 			D3D12_DESCRIPTOR_RANGE instanceRange{};
 			std::array<D3D12_ROOT_PARAMETER, 2> parameters{};
 			const D3D12_ROOT_SIGNATURE_DESC root = MakeInstancedDepthRoot(instanceRange, parameters);
-			GraphicsPipelineDesc desc = MakeDepthDesc(inputLayout);
+			GraphicsPipelineDesc desc = MakeDepthDesc(inputLayout, cullMode);
 			desc.debugName = L"ShadowCaster.Instanced.Depth";
 			desc.shaders.vertexShader.blob = vs;
-			instancedDepthPipeline_ = factory.CreateGraphicsPipeline(desc, root);
+			destination = factory.CreateGraphicsPipeline(desc, root);
 		}
 
-		void CreateObjectPointPipeline(PipelineFactory& factory, const D3D12_INPUT_LAYOUT_DESC& inputLayout, const ComPtr<IDxcBlob>& vs, const ComPtr<IDxcBlob>& ps)
+		void CreateObjectPointPipeline(PipelineFactory& factory, const D3D12_INPUT_LAYOUT_DESC& inputLayout, const ComPtr<IDxcBlob>& vs, const ComPtr<IDxcBlob>& ps, MaterialCullMode cullMode, PipelineBundle& destination)
 		{
 			std::array<D3D12_ROOT_PARAMETER, 2> parameters{};
 			const D3D12_ROOT_SIGNATURE_DESC root = MakeObjectPointRoot(parameters);
-			GraphicsPipelineDesc desc = MakeDepthDesc(inputLayout);
+			GraphicsPipelineDesc desc = MakeDepthDesc(inputLayout, cullMode);
 			desc.debugName = L"ShadowCaster.Object.PointLinear";
 			desc.shaders.vertexShader.blob = vs;
 			desc.shaders.pixelShader.blob = ps;
-			objectPointPipeline_ = factory.CreateGraphicsPipeline(desc, root);
+			destination = factory.CreateGraphicsPipeline(desc, root);
 		}
 
-		void CreateInstancedPointPipeline(PipelineFactory& factory, const D3D12_INPUT_LAYOUT_DESC& inputLayout, const ComPtr<IDxcBlob>& vs, const ComPtr<IDxcBlob>& ps)
+		void CreateInstancedPointPipeline(PipelineFactory& factory, const D3D12_INPUT_LAYOUT_DESC& inputLayout, const ComPtr<IDxcBlob>& vs, const ComPtr<IDxcBlob>& ps, MaterialCullMode cullMode, PipelineBundle& destination)
 		{
 			D3D12_DESCRIPTOR_RANGE instanceRange{};
 			std::array<D3D12_ROOT_PARAMETER, 3> parameters{};
 			const D3D12_ROOT_SIGNATURE_DESC root = MakeInstancedPointRoot(instanceRange, parameters);
-			GraphicsPipelineDesc desc = MakeDepthDesc(inputLayout);
+			GraphicsPipelineDesc desc = MakeDepthDesc(inputLayout, cullMode);
 			desc.debugName = L"ShadowCaster.Instanced.PointLinear";
 			desc.shaders.vertexShader.blob = vs;
 			desc.shaders.pixelShader.blob = ps;
-			instancedPointPipeline_ = factory.CreateGraphicsPipeline(desc, root);
+			destination = factory.CreateGraphicsPipeline(desc, root);
 		}
 
 	private:
-		PipelineBundle objectDepthPipeline_{};
-		PipelineBundle instancedDepthPipeline_{};
-		PipelineBundle objectPointPipeline_{};
-		PipelineBundle instancedPointPipeline_{};
+		std::array<PipelineBundle, 3> objectDepthPipelines_{};
+		std::array<PipelineBundle, 3> instancedDepthPipelines_{};
+		std::array<PipelineBundle, 3> objectPointPipelines_{};
+		std::array<PipelineBundle, 3> instancedPointPipelines_{};
 	};
 }
