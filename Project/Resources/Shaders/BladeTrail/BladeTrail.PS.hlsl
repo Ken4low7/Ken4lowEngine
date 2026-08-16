b1f6ef52806d294bd086d9ff3563e047f514e502
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-16 21:39:45 +0900

    BladeTrail: add ribbon pixel shader

---
+++ Project/Resources/Shaders/BladeTrail/BladeTrail.PS.hlsl
@@ -0,0 +1,20 @@
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+struct PSInput
+{
+    float4 position : SV_POSITION;
+    float2 texcoord : TEXCOORD0;
+    float4 color : COLOR0;
+};
+
+float4 main(PSInput input) : SV_TARGET0
+{
+    float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    float4 outputColor = textureColor * input.color;
+    if (outputColor.a < 0.001f)
+    {
+        discard;
+    }
+    return outputColor;
+}
