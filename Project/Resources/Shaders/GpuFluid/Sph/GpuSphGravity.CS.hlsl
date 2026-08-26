7a6fd5a0e3040041fe57434e5299c3d09b43ab6f
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-26 12:40:44 +0900

    fix(sph): limit gravity UAV writes to velocity

--- Project/Resources/Shaders/GpuFluid/Sph/GpuSphGravity.CS.hlsl
+++ Project/Resources/Shaders/GpuFluid/Sph/GpuSphGravity.CS.hlsl
@@ -9,8 +9,6 @@ void main(uint3 dispatchThreadId : SV_DispatchThreadID)
         return;
     }
 
-    GpuSphParticle particle = gParticles[index];
     // W5.2では外力として重力だけを速度へ積分する。
-    particle.velocity += gSph.gravity * gSph.deltaTime;
-    gParticles[index] = particle;
+    gParticles[index].velocity += gSph.gravity * gSph.deltaTime;
 }
