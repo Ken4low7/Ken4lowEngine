0f7ff63b1f392fe1054837fe799d69a421526b32
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-26 20:31:10 +0900

    feat(editor): expose W9 rigidbody SPH diagnostics

--- Project/Engine/Editor/EditorLevelOverlay.h
+++ Project/Engine/Editor/EditorLevelOverlay.h
@@ -10,6 +10,7 @@
 #include "EditorSceneDeferredController.h"
 #include "EditorWindowManager.h"
 #include "GpuFluidDiagnosticsPanel.h"
+#include "GpuSphRigidbodyInteractionDiagnosticsPanel.h"
 #include "GpuVolumetricFluidDiagnosticsPanel.h"
 
 #include <string>
@@ -27,7 +28,8 @@ namespace Ken4lowEngine
 		EditorWindowManager* windowManager = EditorWindowManager::GetInstance();
 		EditorDiagnosticsPanel::GetInstance()->Draw();
 		EditorProfilerPanel::GetInstance()->Draw(windowManager->GetSceneManager(), &windowManager->GetPerformanceMonitor());
-		GpuFluidDiagnosticsPanel::GetInstance()->Draw(); // Phase16診断はF12の独立Panelとして既存Editor Overlayの1回/Frame入口へ接続する。
+		GpuFluidDiagnosticsPanel::GetInstance()->Draw(); // Fluid/SPH診断はF7の独立Panelとして既存Editor Overlayの1回/Frame入口へ接続する。
+		GpuSphRigidbodyInteractionDiagnosticsPanel::GetInstance()->Draw(); // W9 SPH↔Rigidbody双方向InteractionをF7で同時診断する。
 		GpuVolumetricFluidDiagnosticsPanel::GetInstance()->Draw(); // Phase17のTexture3D Solver診断はF8で独立表示する。
 
 		EditorLevelService* levelService = EditorLevelService::GetInstance();
