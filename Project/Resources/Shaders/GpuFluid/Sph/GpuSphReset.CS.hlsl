4350a86630df1bd831705230eca0f1e7527960ee
Ken4low7 <knt411sirokuro@gmail.com>
2026-08-26 12:29:37 +0900

    feat(sph): add W5 particle reset shader

---
+++ Project/Resources/Shaders/GpuFluid/Sph/GpuSphReset.CS.hlsl
@@ -0,0 +1,23 @@
+#include "GpuSphCommon.hlsli"
+
+[numthreads(128, 1, 1)]
+void main(uint3 dispatchThreadId : SV_DispatchThreadID)
+{
+    const uint index = dispatchThreadId.x;
+    if (!GpuSphIsActiveParticle(index))
+    {
+        return;
+    }
+
+    const uint xy = max(gSph.spawnDimX * gSph.spawnDimY, 1u);
+    const uint x = index % max(gSph.spawnDimX, 1u);
+    const uint y = (index / max(gSph.spawnDimX, 1u)) % max(gSph.spawnDimY, 1u);
+    const uint z = (index / xy) % max(gSph.spawnDimZ, 1u);
+
+    GpuSphParticle particle = (GpuSphParticle)0;
+    particle.position = gSph.spawnOrigin + float3(x, y, z) * gSph.spawnSpacing;
+    particle.predictedPosition = particle.position;
+    particle.density = gSph.targetDensity;
+    gParticles[index] = particle;
+    gScratch[index] = 0.0f.xxxx;
+}
